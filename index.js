78fb3c7a7fc01c2bbd207730f8a9482a4cd36b8f
Saffah Khatib <skhatib1910@gmail.com>
2023-08-01 16:45:15 +0200

    intial commit

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
+import bodyParser from 'body-parser';
+import SettingsBill from './settings.js';
+
 const app = express();
+const settingsBill = SettingsBill();
 
-app.get('/', function(req,res){
-    res.send('Settings Bill App')
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+// app.set('views', './views');
+
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.render('index')
 });
 
-app.post('./settings', function(req, res){
+app.post('/settings', function (req, res) {
+   
+settingsBill.setSettings({
+    callCost : req.body.callCost,
+    smsCost : req.body.smsCost,
+    warningLevel : req.body.warningLevel,
+    criticalLevel : req.body.criticalLevel,
+});
+console.log(settingsBill.getSettings());
+
+
+    res.redirect('/');
+});
+
+app.post('./action', function (req, res) {
 
 });
 
-app.post('./action', function(req, res){
-
-});
-
-app.get('./actions', function(req, res){
+app.get('./actions', function (req, res) {
 
 });
 //this will set call or sms cost
-app.get('./actions/:type', function(req, res){
+app.get('./actions/:type', function (req, res) {
 
 });
 
@@ -25,6 +51,6 @@ app.get('./actions/:type', function(req, res){
 //make PORT a variable so that it can be chnaged in terminal
 const PORT = process.env.PORT || 3011;
 
-app.listen(PORT, function(){
-console.log("App started at port" , PORT);
+app.listen(PORT, function () {
+    console.log("App started at port", PORT);
 });
